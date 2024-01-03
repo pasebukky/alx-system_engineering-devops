9f109eadc04c91bdc648ce12bd6af2954373678c
pasebukky <olubukunolapase@gmail.com>
2024-01-03 23:31:08 +0100

    Compile text

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,5 +3,5 @@ SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
 RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
 FLAG = ARGV[0].scan(/flags:(.*?)\]/).join
 
-text_msg = SENDER + "," + RECEIVER + "," + FLAGS
+text_msg = "#{SENDER},#{RECEIVER},#{FLAGS}"
 puts text_msg
