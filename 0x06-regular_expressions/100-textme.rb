870937c0cd84307893ed45c0c7e4ef4ec00dc0f1
pasebukky <olubukunolapase@gmail.com>
2024-01-03 23:40:08 +0100

    Correct flag typo

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
 RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
-FLAG = ARGV[0].scan(/flags:(.*?)\]/).join
+FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
 
 text_msg = "#{SENDER},#{RECEIVER},#{FLAGS}"
 puts text_msg
