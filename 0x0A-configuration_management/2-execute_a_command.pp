d7ed437db6bcfc3afc88a0d0773ce4b1bc32ef46
pasebukky <olubukunolapase@gmail.com>
2024-02-24 05:30:31 +0100

    correction to code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Use puppet to create a manifest that kills a process named killmenow
 
 exec { 'killmenow':
-  path        => '/usr/bin',
-  command     => 'pkill -f killmenow',
-  refreshonly => true,
+  path    => '/usr/bin',
+  command => 'pkill -SIGTERM killmenow',
+  returns => [0, 1],
 }
