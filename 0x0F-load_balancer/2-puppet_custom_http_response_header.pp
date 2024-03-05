d436b271995cda2b1b2c34c3d5138914af001e21
pasebukky <olubukunolapase@gmail.com>
2024-03-05 23:19:03 +0100

    Update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,24 @@
 # Puppet script to create a custom HTTP header response
 
+exec { 'update':
+  command  => '/usr/bin/apt-get -y update',
+}
+
 package { 'nginx':
-  ensure => 'installed',
+  ensure  => 'installed',
+  require => Exec['update'],
+}
+
+exec { 'add_header':
+  environment => ["HOST=${fqdn}"],
+  command     => '/bin/sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  before      => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => Exec['add_header'],
 }
 
 file { '/var/www/html/index.html':
@@ -16,18 +33,6 @@ file { '/var/www/html/404.html':
 
 file { '/etc/nginx/sites-enabled/default':
   ensure  => 'present',
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-}
-
-# Custom HTTP header configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => 'present',
-  content => "server_name _;\n\tadd_header X-Served-By $hostname;\n\trewrite /redirect_me https://google.com permanent;",
+  content => "server {\n\tserver_name _;\n\trewrite /redirect_me https://google.com permanent;\n}",
   notify  => Service['nginx'],
 }
