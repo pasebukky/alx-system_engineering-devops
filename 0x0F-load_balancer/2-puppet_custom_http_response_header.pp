9b3213ed2b356f2f1b1b819cd63a119668a7ae13
pasebukky <olubukunolapase@gmail.com>
2024-03-06 00:01:19 +0100

    Update to code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,43 +1,42 @@
 # Puppet script to create a custom HTTP header response
 
+# Ensure the system is updated
 exec { 'update':
   command  => '/usr/bin/apt-get -y update',
 }
 
+# Install Nginx
 package { 'nginx':
   ensure  => 'installed',
   require => Exec['update'],
 }
 
-exec { 'add_header':
-  environment => ["HOST=${fqdn}"],
-  command     => '/bin/sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
-  before      => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure     => 'running',
-  enable     => true,
-  subscribe  => Exec['add_header'],
-}
-
+# Create the index.html page
 file { '/var/www/html/index.html':
   content => 'Hello World!',
   ensure  => 'present',
 }
 
+# Create the 404.html page
 file { '/var/www/html/404.html':
   content => 'Ceci n\'est pas une page',
   ensure  => 'present',
 }
 
-# Remove conflicting server block
+# Modify the Nginx configuration file
 file { '/etc/nginx/sites-enabled/default':
-  ensure  => 'present',
   content => "server {
     server_name _;
     rewrite /redirect_me https://google.com permanent;
     add_header X-Served-By $hostname;
   }",
+  ensure  => 'present',
   notify  => Service['nginx'],
+}
+
+# Ensure the Nginx service is running
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-enabled/default'],
 }
