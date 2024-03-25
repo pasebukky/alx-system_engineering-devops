6453475f1087d0e0dac4e0ffcc2af88b7ca7d3eb
pasebukky <olubukunolapase@gmail.com>
2024-03-25 14:22:36 +0100

    Record all tasks that are owned by this employee in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Python script that records all tasks that are owned by this employee
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+
+    user_id = sys.argv[1]
+    user_response = requests.get(base_url + "users/{}".format(user_id))
+    user_info = user_response.json()
+
+    todos_response = requests.get(base_url + "todos",
+                                  params={"userId": user_id})
+    todos_list = todos_response.json()
+
+    csv_filename = f"{user_id}.csv"
+
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for task in todos_list:
+            csv_writer.writerow([user_id, user_info.get("name"),
+                                 task.get("completed"), task.get("title")])
