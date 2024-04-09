d15e6ff0926119ccc5ed520a250f38ea53a788fd
pasebukky <olubukunolapase@gmail.com>
2024-04-09 14:41:54 +0100

    Allow redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers for a particular subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 302:
-        return 0
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
