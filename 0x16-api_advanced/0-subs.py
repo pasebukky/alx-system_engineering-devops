9b0fe2b576dedbe0595051b641bde5dc82691024
pasebukky <olubukunolapase@gmail.com>
2024-04-09 14:40:55 +0100

    Return invalid subreddits to redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,6 +13,8 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 302:
+        return 0
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
