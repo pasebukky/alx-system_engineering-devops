c182ce077cb4bb14707fb0907a11da09393d95c1
pasebukky <olubukunolapase@gmail.com>
2024-04-10 21:49:45 +0100

    Updated code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,13 @@
 """
 
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers for a particular subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Linux Bukky'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
