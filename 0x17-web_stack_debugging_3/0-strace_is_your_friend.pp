caa28d7da33ac6573e5d49c281f18b16765d9d04
pasebukky <olubukunolapase@gmail.com>
2024-04-10 20:38:03 +0000

    Fix bug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixe bug in 'phpp' extensions to 'php' in WordPress
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
