c2c4c6a9bee5563683abb8cb13fddf58670b3e72
pasebukky <olubukunolapase@gmail.com>
2024-04-16 08:38:22 +0000

    Tasks 0, 1 & README

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Increase number of requests handled by Nginx
+
+exec { 'set_ulimit_to 5000':
+  command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
+
+} -> exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+}
